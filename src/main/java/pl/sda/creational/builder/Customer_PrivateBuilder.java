dcefc8b76ccf3882a6c80032e6efa884c51c215d
Michal <misiek-pl@o2.pl>
2019-02-16 15:18:39 +0100

    second cimmit

---
+++ src/main/java/pl/sda/creational/builder/Customer_PrivateBuilder.java
@@ -0,0 +1,100 @@
+package pl.sda.creational.builder;
+
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import java.time.LocalDate;
+
+public class Customer {
+    private String name;
+    private String lastName;
+    private LocalDate birthDate;
+    private String pesel;
+    private String profession;
+    private String city;
+    private String country;
+
+    private Customer(String name,String lastName){
+        this.name=name;
+        this.lastName=lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+    
+    public static class Builder{
+        private Customer customer;
+
+        public Builder(String name, String lastname){
+            customer=new Customer(name,lastname);
+        }
+
+        public Builder bornAt(LocalDate birthDate) {
+            customer.birthDate = birthDate;
+            return this;
+        }
+
+        public Builder whitPesel(String pesel) {
+            Regex regex = new Regex("\\d{11}");
+            customer.pesel = pesel;
+            return this;
+        }
+
+        public Builder workingAs(String profession) {
+            customer.profession = profession;
+            return this;
+        }
+
+        public Builder livesInCity(String city) {
+            customer.city = city;
+            return this;
+        }
+
+        public Builder livesCountry(String country) {
+            customer.country = country;
+            return this;
+        }
+        
+        public Customer build() {
+            return customer;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                ", pesel='" + pesel + '\'' +
+                ", profession='" + profession + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/pl/sda/creational/singleton/SingletonLogger.java
@@ -0,0 +1,4 @@
+package pl.sda.creational.singleton;
+
+public class SingletonLogger {
+}
